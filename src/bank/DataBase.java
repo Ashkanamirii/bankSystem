133ed6bb2f63213f08ebdb9f6ddbf93643fcff69
Sako05 <salem-k@hotmail.com>
2020-12-04 16:29:35 +0100

    Created a very simple DB method, will make remake it and make it more advanced later today or tomorrow.
    Changed the CSV file layout too.

--- src/bank/DataBase.java
+++ src/bank/DataBase.java
@@ -1,5 +1,7 @@
 package bank;
 
+import java.io.*;
+
 /**
  * Created by Ashkan Amiri
  * Date:  2020-11-29
@@ -11,6 +13,30 @@ public class DataBase {
 
 
     public DataBase(){
+
+        String csVFile = "src/bank/Bank_fil_example.csv";
+        String line ="";
+        String csvSplitBy = ",";
+
+
+        try(BufferedReader br = new BufferedReader(new FileReader(csVFile))){
+
+            while ((line = br.readLine()) != null){
+                String[] current = line.split(csvSplitBy);
+
+                System.out.println("Account: " + current[0] + " Accnr: " + current[1] + " Amount: " + current[2] + " Description: " + current[3] + " Balance: " + current[4] + " Timestamp: " + current[5] );
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        new DataBase();
 
     }
 
